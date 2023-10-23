9fbbf4ce07c16ebf84ce24f4da1d117c7989fcc1
Nehesh Sawarkar <neheshsawarkar.22@stvincentngp.edu.in>
2023-10-23 18:53:23 +0530

    C18

---
+++ Basics/C18.c
@@ -0,0 +1,17 @@
+// WAP to print Fibonacci series in a given range
+
+#include <stdio.h>
+
+void main()
+{
+    int i, n1 = 0, n2 = 1, n3, no;
+    printf("Enter Number: ");
+    scanf("%d", &no);
+    for(i = 0; i < no; i++)
+    {
+        printf("%d\t", n1);
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+    }
+}
