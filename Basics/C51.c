fefa0d8a76618f23381f1d02707369a958a2f286
Nehesh Sawarkar <neheshsawarkar.22@stvincentngp.edu.in>
2023-11-04 10:00:27 +0530

    C51

---
+++ Basics/C51.c
@@ -0,0 +1,12 @@
+// WAP to find length of a string
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[20];
+    printf("Enter String: ");
+    gets(str);
+    printf("Length of String: %d", strlen(str));
+}
