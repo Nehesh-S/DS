36adc823f0d6d025d6e875179d6b30bd0eb64481
Nehesh Sawarkar <neheshsawarkar.22@stvincentngp.edu.in>
2023-11-04 10:01:01 +0530

    C55

---
+++ Basics/C55.c
@@ -0,0 +1,16 @@
+// WAP to convert lowercase string to uppercase
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    char str[20];
+    int i;
+    printf("Enter String: ");
+    gets(str);
+    for(i = 0; i < strlen(str); i++)
+        str[i] = toupper(str[i]);
+    printf("Final String: %s", str);
+}
