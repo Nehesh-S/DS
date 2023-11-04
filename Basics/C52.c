334684846ae33ac2763559ef2010655ee654b534
Nehesh Sawarkar <neheshsawarkar.22@stvincentngp.edu.in>
2023-11-04 10:00:37 +0530

    C52

---
+++ Basics/C52.c
@@ -0,0 +1,13 @@
+// WAP to copy one string to another string
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[20], res[20];
+    printf("Enter String: ");
+    gets(str);
+    strcpy(res, str);
+    printf("str: %s\nres: %s", str, res);
+}
