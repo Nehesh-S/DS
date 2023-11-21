add14bed079998bb9620c33a631c15488b4cfc42
Nehesh Sawarkar <nehesh07@gmail.com>
2023-11-21 21:23:41 +0530

    Scenario 2

--- SBQ/S2.c
+++ SBQ/S2.c
@@ -5,47 +5,38 @@
 
 typedef struct
 {
-    char item[10];
+    char name[10];
+    char uid[5];
+    int price;
     int qty;
 }product;
 
 void insertion_sort(product list[], int len)
 {
-    int i, j;
-    product tmp;
-    for(i = 1; i < len; i++)
-    {
-        for(j = 0; j < i; j++)
-        {
-            if(list[j].qty > list[i].qty)
-            {
-                tmp = list[j];
-                list[j] = list[i];
-                list[i] = tmp;
-            }
-        }
-    }
+
 }
 
 int main()
 {
-    product list[MAX];
+    product clothes[MAX];
     int i, len;
-    printf("Enter Item Name and Quantity:\n(Max 10, Type 'Exit' to finish)\n");
+    printf("Enter Item Name, UID, Price and Quantity:\n(Max 10, Type 'Exit' to finish)\n");
     for(i = 0; i < MAX; i++)
     {
-        scanf("%s", list[i].item);
-        if(strcmp(list[i].item, "Exit") == 0)
+        scanf("%s", clothes[i].name);
+        if(strcmp(clothes[i].name, "Exit") == 0)
         {
             len = i;
             break;
         }
-        scanf("%d", &list[i].qty);
+        scanf("%s", clothes[i].uid);
+        scanf("%d", &clothes[i].price);
+        scanf("%d", &clothes[i].qty);
     }
 
-    insertion_sort(list, len);
+    insertion_sort(clothes, len);
 
-    printf("\nSorted List by Quantity:\n");
+    printf("\nSorted Inventory by Price:\n");
     for (i = 0; i < len; i++)
         printf("%s\t%d\n", list[i].item, list[i].qty);
 }
