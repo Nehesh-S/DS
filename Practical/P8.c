f01beb3a577dfabe319d953a1c89a9ead0a36ab6
Nehesh Sawarkar <nehesh07@gmail.com>
2023-11-25 14:00:28 +0530

    Insert

--- Practical/P8.c
+++ Practical/P8.c
@@ -8,6 +8,27 @@ typedef struct node
     struct node* right;
 }node;
 
+node* add(node* root, char name[], int id)
+{
+    if(root == NULL)
+    {
+        node* tmp = (node*)malloc(sizeof(node));
+        if(tmp == NULL)
+        {
+            printf("Cannot Allocate Memory\n");
+            return;
+        }
+        strcpy(tmp->name, name);
+        tmp->id = id;
+        tmp->left = tmp->right = NULL;
+        return tmp;
+    }
+    else if(id > root->id)
+        root = add(root->right, name, id);
+    else if(id < root->id)
+        root = add(root->left, name, id);
+}
+
 int main()
 {
     node* root;
@@ -23,7 +44,7 @@ int main()
             case 1:
                 printf("Enter Employee Name and ID:\n");
                 scanf("%s %d", name, &id);
-                add(root, name, id);
+                root = add(root, name, id);
                 break;
             case 2:
                 printf("Enter Employee ID: ");
