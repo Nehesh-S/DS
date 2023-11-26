f91dd7590432aca005be4d44c1274a48cf201bf6
Nehesh Sawarkar <nehesh07@gmail.com>
2023-11-26 11:47:18 +0530

    P8

--- Practical/P8.c
+++ Practical/P8.c
@@ -37,7 +37,7 @@ void search(node* root, int id)
 {
     if(root == NULL)
     {
-        printf("Employee Not Found!");
+        printf("Employee Not Found!\n");
         return;
     }
     else if(root->id == id)
