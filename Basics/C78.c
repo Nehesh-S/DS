e053493b8cbf2fe01d5fc990e4f49a72e6c8d75b
Nehesh Sawarkar <nehesh07@gmail.com>
2023-12-04 14:52:13 +0530

    C78

---
+++ Basics/C78.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int find(const char *str, const char *word) {
+    int strLen = strlen(str);
+    int wordLen = strlen(word);
+    for (int i = 0; i <= strLen - wordLen; i++) {
+        int j;
+        for (j = 0; j < wordLen; j++) {
+            if (str[i + j] != word[j]) {
+                break;
+            }
+        }
+        if (j == wordLen) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    char str[50];
+    char word[10];
+    printf("Enter a string: ");
+    gets(str);
+    printf("Enter the word to find: ");
+    scanf("%s", word);
+    int result = find(str, word);
+
+    if (result != -1)
+        printf("First occurrence of '%s' found at index %d\n", word, result);
+    else
+        printf("Word not found in the string.\n");
+    return 0;
+}
