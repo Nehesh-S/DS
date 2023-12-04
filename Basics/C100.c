09686e6093491957e2d02a316054c00ab6184856
Nehesh Sawarkar <nehesh07@gmail.com>
2023-12-04 19:36:54 +0530

    C100

--- Basics/C100.c
+++ Basics/C100.c
@@ -5,28 +5,31 @@ typedef struct{
     int scores[5];
 }student;
 
-int main() {
+int main()
+{
     student students[3];
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         printf("Enter name of student %d: ", i + 1);
         scanf("%s", students[i].name);
 
         printf("Enter scores for 5 subjects for %s:\n", students[i].name);
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < 5; j++)
+        {
             printf("Subject %d: ", j + 1);
             scanf("%d", &students[i].scores[j]);
         }
     }
 
     printf("\nStudent Information:\n");
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         printf("\nStudent %d\n", i + 1);
         printf("Name: %s\n", students[i].name);
         printf("Scores: ");
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < 5; j++)
             printf("%d ", students[i].scores[j]);
-        }
         printf("\n");
     }
 }
