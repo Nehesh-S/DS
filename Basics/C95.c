94d9ca3c3eea87162ddb64d7a565c0e321ab4d97
Nehesh Sawarkar <nehesh07@gmail.com>
2023-12-04 16:10:54 +0530

    C95

---
+++ Basics/C95.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct {
+    char name[10];
+    int id;
+    float salary;
+}employee;
+
+int main() {
+    employee employee1;
+
+    printf("Enter employee details:\n");
+    printf("Name: ");
+    scanf("%s", employee1.name);
+    printf("Employee ID: ");
+    scanf("%d", &employee1.id);
+    printf("Salary: ");
+    scanf("%f", &employee1.salary);
+
+    printf("Employee Details:\n");
+    printf("Name: %s\n", employee1.name);
+    printf("Employee ID: %d\n", employee1.id);
+    printf("Salary: %.2f\n", employee1.salary);
+
+    return 0;
+}
