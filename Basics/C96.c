611cd180f2e723536cd5cbcba5d523a095a606d1
Nehesh Sawarkar <nehesh07@gmail.com>
2023-12-04 19:20:50 +0530

    C96

---
+++ Basics/C96.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+typedef struct{
+    char city[50];
+    char state[50];
+}address;
+
+typedef struct{
+    char name[50];
+    int id;
+    float salary;
+    address add;
+}employee;
+
+int main() {
+    employee employee1;
+
+    printf("Enter employee details:\n");
+    printf("Name: ");
+    scanf("%s", employee1.name);
+    printf("Employee ID: ");
+    scanf("%d", &employee1.id);
+    printf("Salary: ");
+    scanf("%f", &employee1.salary);
+    printf("City: ");
+    scanf("%s", employee1.add.city);
+    printf("State: ");
+    scanf("%s", employee1.add.state);
+
+    printf("\nEmployee Details:\n");
+    printf("Name: %s\n", employee1.name);
+    printf("Employee ID: %d\n", employee1.id);
+    printf("Salary: %.2f\n", employee1.salary);
+    printf("Address: %s, %s\n", employee1.add.city, employee1.add.state);
+}
