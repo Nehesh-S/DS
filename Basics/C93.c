23d7af6486e11ddc9b1db9d5a97324af8917e36f
Nehesh Sawarkar <nehesh07@gmail.com>
2023-12-04 16:06:50 +0530

    C93

---
+++ Basics/C93.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void ref(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    printf("Enter two values: ");
+    scanf("%d %d", &x, &y);
+    
+    int tmp = x;
+    x = y;
+    y = tmp;
+    printf("Values after swapping (Value): x = %d, y = %d\n", x, y);
+
+    ref(&y, &x);
+    printf("Values after swapping (Reference): x = %d, y = %d\n", y, x);
+
+}
