a4d195c05289643d8f689832ba9d2280fbb00ab5
Nehesh Sawarkar <nehesh07@gmail.com>
2023-12-04 16:08:39 +0530

    C94

---
+++ Basics/C94.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct{
+    char name[10];
+    int roll;
+    float marks;
+}student;
+
+int main() {
+    student student1 = {"Nehesh", 45, 95};
+
+    printf("Student Details:\n");
+    printf("Name: %s\n", student1.name);
+    printf("Roll Number: %d\n", student1.roll);
+    printf("Marks: %.2f\n", student1.marks);
+
+    return 0;
+}
